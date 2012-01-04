d71762e6078b6c449431732f179ea112b31430e1
Tim Shannon <tim@townsourced.com>
2012-01-04 17:35:10 -0600

    Started testing the gorest package

---
+++ src/gal3upload/gal3rest/gal_test.go
@@ -0,0 +1,11 @@
+package gal3rest
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRest(t *testing.T) {
+	TestConnection()
+	fmt.Println("test")
+}
---
+++ src/gal3upload/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"gal3rest"
+	"fmt"
+)
+
+func main() {
+	gal3rest.TestConnection()
+	fmt.Println("test")
+}

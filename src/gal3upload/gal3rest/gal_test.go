acee9c6869e4996e566077b63e681caf1f27f508
Tim Shannon <shannon.timothy@gmail.com>
2012-01-09 17:28:47 -0600

    Started json handling

--- src/gal3upload/gal3rest/gal_test.go
+++ src/gal3upload/gal3rest/gal_test.go
@@ -3,17 +3,9 @@ package gal3rest
 import (
 	"fmt"
 	"testing"
-	"io/ioutil"
 )
 
 func TestRest(t *testing.T) {
 	response := TestConnection("http://www.timandmeg.net/gallery3/index.php")
-	fmt.Println("content Length: ", response.ContentLength)
-	b, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	if err != nil {
-		fmt.Println("err")
-	}
-	fmt.Println(string(b))
-
+	fmt.Println(response)
 }

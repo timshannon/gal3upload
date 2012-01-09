e7ee197dcbafcf68db695b0c411bc13d61de09c4
Tim Shannon <shannon.timothy@gmail.com>
2012-01-09 17:12:07 -0600

    Now able to read the body of the get request

--- src/gal3upload/gal3rest/gal3rest.go
+++ src/gal3upload/gal3rest/gal3rest.go
@@ -3,25 +3,23 @@ package gal3rest
 import (
 	"log"
 	"http"
-	"os"
 	"strings"
 )
 
 func TestConnection(url string) *http.Response {
 	client := new(http.Client)
-	reader := new(strings.Reader)
-	req, _ := http.NewRequest("GET", url+"/rest/item/1", reader)
+	reader := new(strings.Reader) //TODO: build actuall content
+	if url[:1] != "/" {
+		url += "/"
+	}
+	req, _ := http.NewRequest("GET", url+"rest/item/1", reader)
 	req.Header.Set("X-Gallery-Request-Method", "GET")
 	req.Header.Set("X-Gallery-Request-Key", "79daf60695177e16ff2480f8338b5fcc")
 	response, err := client.Do(req)
+
 	if err != nil {
 		log.Panic("Error connecting to: "+url+" Error: ", err)
 	}
+
 	return response
-}
-
-type GalleryRequest struct{}
-
-func (r GalleryRequest) Read(p []byte) (n int, err os.Error) {
-	return 0, nil
 }
--- src/gal3upload/gal3rest/gal_test.go
+++ src/gal3upload/gal3rest/gal_test.go
@@ -3,9 +3,17 @@ package gal3rest
 import (
 	"fmt"
 	"testing"
+	"io/ioutil"
 )
 
 func TestRest(t *testing.T) {
 	response := TestConnection("http://www.timandmeg.net/gallery3/index.php")
-	fmt.Println()
+	fmt.Println("content Length: ", response.ContentLength)
+	b, err := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	if err != nil {
+		fmt.Println("err")
+	}
+	fmt.Println(string(b))
+
 }

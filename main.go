0ab697e24e3c6c53113fd7b7d6c13287e21bdf0e
Tim Shannon <shannon.timothy@gmail.com>
2012-01-11 17:07:45 -0600

    Settled on nil interface to work with json package.  Started building the client interface to work with the rest data.

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"gal3rest"
+	//	"gal3rest"
 	"fmt"
 )
 
 func main() {
-	gal3rest.TestConnection("http://www.timandmeg.net/gallery3/index.php")
+	//gal3rest.TestConnection("http://www.timandmeg.net/gallery3/index.php")
 	fmt.Println("test")
-	
+
 }

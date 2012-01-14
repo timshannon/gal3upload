d3541ec60fb30199d7790d1ce810b94531226ace
Tim Shannon <tim@townsourced.com>
2012-01-14 13:48:58 -0600

    My original plan of working off of the rest item id fell apart, because it looks like they don't actually include the item id in the entity information in the rest data.  They have a spot for it, but it's always 0.  I'll have to use direct urls instead.

--- gal3rest/gal_test.go
+++ gal3rest/gal_test.go
@@ -3,17 +3,29 @@ package gal3rest
 import (
 	"fmt"
 	"testing"
+	"reflect"
 )
 
 func TestRest(t *testing.T) {
 	client := new(Client)
 	client.Url = "http://www.timandmeg.net/gallery3/index.php"
 	client.APIKey = "79daf60695177e16ff2480f8338b5fcc"
-	album := client.GetAlbum(1)
+	album := client.GetAlbum(client.GetUrlFromId(1))
 	fmt.Println("Number of Photos: ", len(album.Photos))
 	fmt.Println("Number of Albums: ", len(album.Albums))
-	fmt.Println(album)
-	//for a := range album.Albums {
-	//fmt.Println(album.Albums[a])
-	//}
+	for i := range album.Albums {
+		fmt.Println("Album: ", album.Albums[i])
+	}
+	PrintEntity(&album.Entity)
+}
+
+func PrintEntity(entity *Entity) {
+	ref := reflect.ValueOf(entity).Elem()
+	entityType := ref.Type()
+	for i := 0; i < ref.NumField(); i++ {
+		field := ref.Field(i)
+		fmt.Printf("%s: %s  %v\n",
+			entityType.Field(i).Name,
+			field.Type(), field.Interface())
+	}
 }

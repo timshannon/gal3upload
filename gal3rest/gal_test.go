61a32ddc952f72371e9c8320fc70fdcc02039a73
Tim Shannon <shannon.timothy@gmail.com>
2012-01-13 23:32:47 -0600

    Got the basic request stuff in place.  Next to try some to build the put or upload requests.

--- gal3rest/gal_test.go
+++ gal3rest/gal_test.go
@@ -9,15 +9,11 @@ func TestRest(t *testing.T) {
 	client := new(Client)
 	client.Url = "http://www.timandmeg.net/gallery3/index.php"
 	client.APIKey = "79daf60695177e16ff2480f8338b5fcc"
-
-	printMembers(client, 39380)
-}
-
-func printMembers(client *Client, id int) {
-	data := client.GetRESTItem(id)
-	members := data.GetMembers()
-	fmt.Println(members)
-	for m := range members {
-		fmt.Println(members[m])
-	}
+	album := client.GetAlbum(1)
+	fmt.Println("Number of Photos: ", len(album.Photos))
+	fmt.Println("Number of Albums: ", len(album.Albums))
+	fmt.Println(album)
+	//for a := range album.Albums {
+	//fmt.Println(album.Albums[a])
+	//}
 }

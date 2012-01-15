79a04c4486b2d75cb05f75ac62d323736b84ec85
Tim Shannon <tim@townsourced.com>
2012-01-14 18:32:41 -0600

    Started on the Create Album requests

--- gal3rest/gal_test.go
+++ gal3rest/gal_test.go
@@ -7,16 +7,18 @@ import (
 )
 
 func TestRest(t *testing.T) {
-	client := new(Client)
-	client.Url = "http://www.timandmeg.net/gallery3/index.php"
-	client.APIKey = "79daf60695177e16ff2480f8338b5fcc"
+	client := NewClient("http://www.timandmeg.net/gallery3/index.php",
+		"79daf60695177e16ff2480f8338b5fcc")
 	album := client.GetAlbum(client.GetUrlFromId(1))
 	fmt.Println("Number of Photos: ", len(album.Photos))
 	fmt.Println("Number of Albums: ", len(album.Albums))
 	for i := range album.Albums {
 		fmt.Println("Album: ", album.Albums[i])
 	}
-	PrintEntity(&album.Entity)
+
+	fmt.Println("Creating a new album")
+	client.CreateAlbum("Test Album", "Test Album",
+		client.GetUrlFromId(1))
 }
 
 func PrintEntity(entity *Entity) {
